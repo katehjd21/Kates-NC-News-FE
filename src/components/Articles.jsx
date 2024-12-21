7b5e02e2d82840acb4553990d15e737ee318e747
Kate Drew <katedrew2103@hotmail.co.uk>
2024-12-21 16:25:22 +0000

    sorted pagination

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { getArticles } from "../utils/api";
 import ArticlesSortBy from "./ArticlesSortBy";
+import ArticlesOrder from "./ArticlesOrder";
 import ArticlesList from "./ArticlesList";
 import { useSearchParams } from "react-router-dom";
+import Pagination from "./Pagination";
 
 function FetchArticles() {
-  const [articles, setArticles] = useState([]);
+  const [articlesList, setArticlesList] = useState([]);
   const [isLoading, setIsLoading] = useState(null);
   const [hasError, setHasError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+  const [limit, setLimit] = useState(9);
   const topicQuery = searchParams.get("topic");
   const sortByQuery = searchParams.get("sort_by") || "created_at";
   const orderQuery = searchParams.get("order") || "desc";
@@ -27,17 +32,17 @@ function FetchArticles() {
 
   useEffect(() => {
     setIsLoading("Articles are loading...");
-    getArticles(topicQuery, sortByQuery, orderQuery)
-      .then((articlesData) => {
-        setArticles(articlesData);
+    getArticles(topicQuery, sortByQuery, orderQuery, page, limit)
+      .then(({ articles, total_count }) => {
+        setArticlesList(articles);
+        setTotalCount(total_count);
         setIsLoading(null);
       })
-      .catch((err) => {
-        console.log("Error fetching articles:", err);
+      .catch(() => {
         setHasError("Failed to load articles. Please try again later!");
         setIsLoading(null);
       });
-  }, [topicQuery, sortByQuery, orderQuery]);
+  }, [topicQuery, sortByQuery, orderQuery, page, limit]);
 
   return (
     <section>
@@ -45,16 +50,17 @@ function FetchArticles() {
         {topicQuery ? `Articles on ${topicQuery}` : "All Articles"}
       </h2>
       <ArticlesSortBy setSortBy={setSortBy} currentSortBy={sortByQuery} />
-      <button className="order-btn" onClick={() => setSortOrder("asc")}>
-        Ascending
-      </button>
-      <button className="order-btn" onClick={() => setSortOrder("desc")}>
-        Descending
-      </button>
+      <ArticlesOrder setSortOrder={setSortOrder} />
       <ArticlesList
-        articles={articles}
+        articlesList={articlesList}
         isLoading={isLoading}
         hasError={hasError}
+      />
+      <Pagination
+        totalCount={totalCount}
+        setPage={setPage}
+        page={page}
+        limit={limit}
       />
     </section>
   );
